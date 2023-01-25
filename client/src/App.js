bd212a6839123179a1cc41a6e3f2151728d479fe
Akash RP <akashrp512@gmail.com>
2023-01-25 18:49:54 +0530

    client setup

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  UseLocation,
+  useLocation
+} from 'react-router-dom';
+import Home from './scenes/home/Home'
+
+const ScrollToTop = ( ) =>{
+  const {pathname} =useLocation();
+
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[pathname])
+  return null;
+}
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+//add
