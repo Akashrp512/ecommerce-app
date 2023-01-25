8f3514c085aa5e354f9d1300acad52bf08cc5076
Akash RP <akashrp512@gmail.com>
2023-01-25 21:57:34 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,11 @@ import {
   UseLocation,
   useLocation
 } from 'react-router-dom';
-import Home from './scenes/home/Home'
+import Home from './scenes/home/Home';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
 
 const ScrollToTop = ( ) =>{
   const {pathname} =useLocation();
@@ -23,9 +27,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='item/:itemId' element={<ItemDetails />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/success' element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,23 @@ import './index.css';
 import App from './App';
 import {ThemeProvider} from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
-import {theme} from './theme'
+import {theme} from './theme';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import CartReducer from './state'
+
+const store= configureStore({
+  reducer:{cart: CartReducer},
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
   <ThemeProvider theme={theme}>
   <CssBaseline />
     <App />
     </ThemeProvider> 
+  </Provider>
   </React.StrictMode>
 );
